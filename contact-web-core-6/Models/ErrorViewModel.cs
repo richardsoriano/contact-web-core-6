39bd96d040df45546db968d81316786eeb0ab0d4
Richard Soriano <soriano.richard2020@gmail.com>
2024-09-22 10:46:02 -0700

    initial commit

---
+++ contact-web-core-6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace contact_web_core_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
